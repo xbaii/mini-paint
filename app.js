4b32ef821aaabab0a3b4e090376b4fd6cdd4de57
Bailey Leising <bleising10@gmail.com>
2015-01-15 15:38:26 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+	var color = 'white';
+	var colors = 'white green red blue yellow';
+
+	$('.box').on('click', function() {
+		$(this).removeClass(colors);
+		$(this).addClass(color);
+	});
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(colors);
+	});
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
+	})
+
+
+
+	$('#red').on('click', function() {
+		color = 'red';
+	})
+	$('#blue').on('click', function() {
+		color = 'blue';
+	})
+	$('#green').on('click', function() {
+		color = 'green';
+	})
+	$('#yellow').on('click', function() {
+		color = 'yellow';
+	})
+	$('#white').on('click', function() {
+		color = 'white';
+	})
+});
